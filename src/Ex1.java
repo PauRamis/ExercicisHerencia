8a7c60fbd869c08f55c73261c8552453864b2380
Your Name <you@example.com>
2022-02-23 18:47:51 +0100

    Initial commit

---
+++ src/Ex1.java
@@ -0,0 +1,41 @@
+public class Ex1 {
+
+    //1- Considera les classes:
+/*
+    public class ClassA {
+        public void methodOne(int i) {
+        }
+
+        public void methodTwo(int i) {
+        }
+
+        public static void methodThree(int i) {
+        }
+
+        public static void methodFour(int i) {
+        }
+    }
+
+    public class ClassB extends ClassA {
+        public static void methodOne(int i) {
+        }
+
+        public void methodTwo(int i) {
+        }
+
+        public void methodThree(int i) {
+        }
+
+        public static void methodFour(int i) {
+        }
+    }
+
+a) Quin mètode fa un «override» en la superclasse?
+    methodTwo
+b) Quin mètode amaga un mètode a la superclasse?
+    methodFour
+d) Són correctes tots els mètodes?
+ només el segon i el cuart
+ */
+
+}
---
+++ src/Ex2.java
@@ -0,0 +1,6 @@
+public class Ex2 {
+    //2- Implementa una classe anomenada «Circle», que serà la super-classe de «Cylinder».
+    //Aquí teniu el diagrama:
+
+
+}
