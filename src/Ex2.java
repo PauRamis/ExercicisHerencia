99bb2c67ef35ccc3afc1ef1459e113419d6a1004
Your Name <you@example.com>
2022-02-25 18:53:28 +0100

    copy 2 and 3

--- src/Ex2.java
+++ src/Ex2.java
@@ -1,6 +1,61 @@
 public class Ex2 {
     //2- Implementa una classe anomenada «Circle», que serà la super-classe de «Cylinder».
-    //Aquí teniu el diagrama:
+    //• Implementa tots els mètodes especificats al diagrama.
+
+    //• La subclasse «Cylinder» hereda el mètode «getArea()». Fes una sobrecàrrega d'aquest
+    //mètode perquè calculi l'àrea de la superfície del cilindre (2*PI*Rad*Altura + 2*AreaBase).
+
+    //• Implementa el mètode «toString» a la classe «Cylinder» perquè mostri que és una subclasse
+    //de «Circle»
+    public class Circle {
 
 
+        private double radius = 1.0;
+        private String color = "red";
+
+        Circle() {
+        }
+
+        public Circle(double radius) {
+            this.radius = radius;
+        }
+
+        public double getRadius() {
+            return this.radius;
+        }
+
+        public double getArea() {
+            return Math.PI + Math.pow(this.radius, 2);
+        }
+
+        public String toString(){
+            return color;
+        }
+    }
+    public class Cylinder{
+        public Cylinder(){
+
+        }
+
+        public Cylinder(double radius){
+            super(radius);
+        }
+
+        public Cylinder(double radius, double height){
+            super(radius);
+            this.height = height;
+        }
+
+        @Override
+        public double getArea() {
+        double areaCercle = super.getArea;
+        double areaRectangle = this.height * 2 * Math.PI * getRadius;
+        return 2*areaCercle + areaRectangle;
+        }
+
+        @Override
+        public String toString() {
+            return "Cylinder.superclas ";
+        }
+    }
 }
---
+++ src/Ex3.java
@@ -0,0 +1,73 @@
+public class Ex3 {
+    public class shape{
+        private boolean filled = true;
+        private String color = "red";
+
+        public shape(){}
+
+        public shape(boolean filled, String color){
+            this.filled = filled;
+            this.color = color;
+        }
+
+        public boolean isFilled(){
+            return filled;
+        }
+
+        public String getColor(){
+            return color;
+        }
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+
+        @Override
+        public String toString(){
+            return "Shape with color of " +
+                    this.color +
+                    "and" +
+                    (this.filled ? filled);
+        }
+    }
+
+    public class rectangle extends shape{
+        private double with = 1;
+        private double lenght = 1;
+
+        public rectangle(){}
+
+        public rectangle(double with, double lenght){
+            this.with = with;
+            this.lenght = lenght;
+        }
+
+        public rectangle(double with, double lenght, String color, boolean isFilled){
+            this.with = with;
+            this.lenght = lenght;
+            setColor(color);
+        }
+
+        public double getWith() {
+            return with;
+        }
+
+        public void setWith(double with) {
+            this.with = with;
+        }
+
+        public double getLenght() {
+            return lenght;
+        }
+
+        public void setLenght(double lenght) {
+            this.lenght = lenght;
+        }
+        @Override
+        public toString(){
+            return "rectangle with width" + this.with +
+                    "and lenght" + this.lenght +
+                    "with a sublas of " super.toString();
+        }
+    }
+}
